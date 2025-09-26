af96b09492496d2201ae7efe1ee836bd150158f1
saurabhxcod <offical.saurabhsingh@gmail.com>
2025-09-26 11:44:01 +0530

    GraphQL mutations i.e. add , update, delete

--- schema/schema.js
+++ schema/schema.js
@@ -1,4 +1,9 @@
 import { GraphQLSchema , GraphQLObjectType , GraphQLString, GraphQLInt} from "graphql";
+const users=[
+  {id:'1',name:'John Doe',age:28},
+  {id:'2',name:'Jane Doe',age:25},
+  {id:'3',name:'Jim Doe',age:22}
+]
 
 const UserType=new GraphQLObjectType({
   name:'User',
@@ -11,19 +16,15 @@ const UserType=new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-
     user:{
       type:UserType,
       args:{
         id:{type:GraphQLString}
       },
       resolve(parent,args){
-        const users=[
-          {id:'1',name:'John Doe',age:28},
-          {id:'2',name:'Jane Doe',age:25},
-          {id:'3',name:'Jim Doe',age:22}
-        ]
+        console.log(users)
         return users.find(user=>user.id===args.id);
+
       }
     },
 
@@ -37,8 +38,64 @@ const RootQuery = new GraphQLObjectType({
   }
 });
 
+const Mutation = new GraphQLObjectType({
+  name:'Mutation',
+  fields:{
+    addUser:{
+      type:UserType,
+      args:{
+        name:{type:GraphQLString},
+        age:{type:GraphQLInt}
+      },
+        resolve(parent,args){
+          const newUser={
+            id:users.length+1+"",
+            name:args.name,
+            age:args.age
+          }
+          users.push(newUser)
+          console.log(users)
+          return newUser
+        }
+    },
+    updateUser:{
+      type:UserType,
+      args:{
+        id:{type:GraphQLString},
+        name:{type:GraphQLString},
+        age:{type:GraphQLInt}
+      },
+        resolve(parent,args){
+         const user=users.find(user=>user.id===args.id)
+         if(user){
+          user.name=args.name
+          user.age=args.age
+          console.log(users)
+          return user
+         }
+         throw new Error("User not found")
+        }
+    },
+    deleteUser:{
+      type:UserType,
+      args:{
+        id:{type:GraphQLString}
+      },  
+      resolve(parent,args){
+        const index=users.findIndex(user=>user.id===args.id)
+        if(index!==-1){
+          users.splice(index,1)
+          console.log(users)
+          return users[index]
+        }
+        throw new Error("User not found")
+      }
+    }
+  }
+})
 const schema = new GraphQLSchema({
-    query: RootQuery        
+    query: RootQuery,
+    mutation: Mutation
 });  
 
 export default schema;

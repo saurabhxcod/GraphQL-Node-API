7c640fcc7662a55ed0bc49352f4f87ff78948d99
saurabhxcod <offical.saurabhsingh@gmail.com>
2025-09-26 13:33:07 +0530

    Added InputTypes and validations for best pracitces

--- schema/schema.js
+++ schema/schema.js
@@ -1,9 +1,10 @@
-import { GraphQLSchema , GraphQLObjectType , GraphQLString, GraphQLInt} from "graphql";
-const users=[
-  {id:'1',name:'John Doe',age:28},
-  {id:'2',name:'Jane Doe',age:25},
-  {id:'3',name:'Jim Doe',age:22}
-]
+import { GraphQLSchema , GraphQLObjectType , GraphQLString, GraphQLInt, GraphQLList,GraphQLInputObjectType,GraphQLNonNull} from "graphql";
+import User from '../models/User.Model.js';
+// const users=[
+//   {id:'1',name:'John Doe',age:28},
+//   {id:'2',name:'Jane Doe',age:25},
+//   {id:'3',name:'Jim Doe',age:22}
+// ]
 
 const UserType=new GraphQLObjectType({
   name:'User',
@@ -13,28 +14,44 @@ const UserType=new GraphQLObjectType({
     age:{type:GraphQLInt}
   }
 })
+
+const UserInputType=new GraphQLInputObjectType({
+  name:'UserInput',
+  fields:{
+    id:{type:GraphQLString},
+    name:{type:new GraphQLNonNull(GraphQLString)},
+    age:{type:GraphQLInt}
+  }
+})
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
+    users:{
+      type:new GraphQLList(UserType), 
+      resolve(parent,args){
+        return User.find()
+      }
+    },
     user:{
       type:UserType,
       args:{
         id:{type:GraphQLString}
       },
       resolve(parent,args){
-        console.log(users)
-        return users.find(user=>user.id===args.id);
+        // console.log(users)
+        // return users.find(user=>user.id===args.id);
+        return User.findById(args.id)
 
       }
     },
 
 
-    hello: {
-      type: GraphQLString,
-      resolve() {
-        return 'Hello from GraphQL';
-        }
-    }
+    // hello: {
+    //   type: GraphQLString,
+    //   resolve() {
+    //     return 'Hello from GraphQL';
+    //     }
+    // }
   }
 });
 
@@ -44,51 +61,82 @@ const Mutation = new GraphQLObjectType({
     addUser:{
       type:UserType,
       args:{
-        name:{type:GraphQLString},
-        age:{type:GraphQLInt}
+        // name:{type:GraphQLString},
+        // age:{type:GraphQLInt}
+
+        //using input type
+        input:{type:UserInputType}
+
       },
-        resolve(parent,args){
-          const newUser={
-            id:users.length+1+"",
-            name:args.name,
-            age:args.age
+        async resolve(_,{input}){
+          // const newUser={
+          //   id:users.length+1+"",
+          //   name:args.name,
+          //   age:args.age
+          // }
+          // users.push(newUser)
+          // console.log(users)
+          // return newUser
+
+
+          //using dynamic data
+          if(!input.name || input.name.length<3){
+            throw new Error("Name must be at least 3 characters long")
           }
-          users.push(newUser)
-          console.log(users)
-          return newUser
+          
+          // const newUser=new User({name:args.name,age:args.age})
+          const newUser=new User({name:input.name,age:input.age})
+          console.log(newUser)
+          return await newUser.save()
         }
     },
     updateUser:{
       type:UserType,
       args:{
-        id:{type:GraphQLString},
-        name:{type:GraphQLString},
-        age:{type:GraphQLInt}
+        // id:{type:GraphQLString},
+        // name:{type:GraphQLString},
+        // age:{type:GraphQLInt}
+
+        //using input type
+        input:{type:UserInputType}
       },
-        resolve(parent,args){
-         const user=users.find(user=>user.id===args.id)
-         if(user){
-          user.name=args.name
-          user.age=args.age
-          console.log(users)
-          return user
-         }
-         throw new Error("User not found")
+        async resolve(parent,{input}){
+        //  const user=users.find(user=>user.id===args.id)
+        //  if(user){
+        //   user.name=args.name
+        //   user.age=args.age
+        //   console.log(users)
+        //   return user
+        //  }
+        //  throw new Error("User not found")
+        // }
+
+        //using dynamic data
+        const user=await User.findByIdAndUpdate(input.id,{name:input.name,age:input.age},{new:true})
+        console.log(user)
+        return user
         }
     },
     deleteUser:{
       type:UserType,
       args:{
-        id:{type:GraphQLString}
+        // id:{type:GraphQLString}
+        input:{type:UserInputType}
       },  
-      resolve(parent,args){
-        const index=users.findIndex(user=>user.id===args.id)
-        if(index!==-1){
-          users.splice(index,1)
-          console.log(users)
-          return users[index]
-        }
-        throw new Error("User not found")
+      async resolve(parent,{input}){
+      //   const index=users.findIndex(user=>user.id===args.id)
+      //   if(index!==-1){
+      //     users.splice(index,1)
+      //     console.log(users)
+      //     return users[index]
+      //   }
+      //   throw new Error("User not found")
+      // }
+
+      //using dynamic data
+      const user=await User.findByIdAndDelete(input.id)
+      console.log(user)
+      return user
       }
     }
   }
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import schema from './schema/schema.js';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+dotenv.config();
 const app=express();
 const PORT=3000;
 
-
+//MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.log(err));
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
